1555ce2f3e16d9ed20a4a70ca1c64fe393f77511
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-22 17:24:54 +0100

    migration for username user_id and phone_number

---
+++ db/migrate/20210322160728_add_user_id_to_boxes.rb
@@ -0,0 +1,5 @@
+class AddUserIdToBoxes < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :boxes, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_145105) do
+ActiveRecord::Schema.define(version: 2021_03_22_162152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2021_03_22_145105) do
     t.bigint "box_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["box_id"], name: "index_bookings_on_box_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "boxes", force: :cascade do |t|
@@ -32,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_03_22_145105) do
     t.integer "price_per_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_boxes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,9 +46,13 @@ ActiveRecord::Schema.define(version: 2021_03_22_145105) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "phonenumber"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "boxes"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "boxes", "users"
 end
