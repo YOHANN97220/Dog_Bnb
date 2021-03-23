95c4144f68408ad184ec7713ba1787949da482c4
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-23 15:13:54 +0100

    updated controller with action show, new ,Configurated the route to boxes/show and create 4users and 4boxes in the seed file

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,7 +1,15 @@
 class BoxesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @boxes = Box.all
+  end
+
+  def show
+    @box = Box.find(params[:id])
+  end
+
+  def new
+    @box = Box.new
   end
 end
