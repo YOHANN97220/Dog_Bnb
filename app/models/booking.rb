4f0d6fd76d103b9415ec4238df72407e33ff1cab
Daniela Zimmermann <ceridween@protonmail.com>
2021-03-23 12:26:57 +0100

    models done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :box
+
+  validates :start_at, :end_at, presence: true
 end
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,5 @@
 class Box < ApplicationRecord
   belongs_to :user
+
+  validates :name, :address, :price_per_day, presence: true
 end
