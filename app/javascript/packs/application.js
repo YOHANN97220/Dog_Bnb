09a410cd8e3ab6abf2ca08eb4824b63dad5919e9
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-25 14:27:31 +0100

    marker added to map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,10 +28,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
-
-import { initMapbox } from '../plugins/init_mapbox';
-
-document.addEventListener('turbolinks:load', () => {
   initMapbox();
-})
+});
