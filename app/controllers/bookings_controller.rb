268748cea3a5df6178e68fd3f69e03b391c40fa8
Alexia Ashton <ashtonalexia@gmail.com>
2021-03-25 11:10:22 +0100

    wanting to be up to date

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+
+  def new
+    @box = Box.find(params[:box_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @box = Box.find(params[:box_id])
+    @booking = Booking.new(booking_params)
+    @booking.box = @box
+    if @booking.save
+      redirect_to booking_path(@box)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_at, :end_at, :box_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'boxes/users'
   get 'boxes/bookings'
   root to: 'pages#home'
-  resources :boxes, only: [:index, :show, :new, :create]
+  resources :boxes, only: [ :index, :show, :new, :create ] do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :booking, only: [ :index, :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
